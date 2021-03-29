f5b6982c5275e21211806b34e0e50caa80ab05ef
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2021-03-30 07:34:20 +0900

    Migrationを有効にした （Enable-Migrationsコマンド）

---
+++ Codefirstnewconnectionstring2/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Codefirstnewconnectionstring2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Codefirstnewconnectionstring2.BloggingContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Codefirstnewconnectionstring2.BloggingContext";
+        }
+
+        protected override void Seed(Codefirstnewconnectionstring2.BloggingContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
